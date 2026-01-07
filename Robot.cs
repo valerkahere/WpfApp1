61d46770d359f1e30766aab1f024b71150354c91
Valerii Navalnyi <S00274450@atu.ie>
2026-01-07 10:11:03 +0000

    Complete Q1 (c)

---
+++ Robot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    abstract class Robot 
+    {
+        public string RobotName { get; set; }
+        public double PowerCapacityKWH { get; set; }
+        public double CurrentPowerKWH { get; set; }
+        //public Robot(string name, int batteryLevel)
+        //{
+        //    Name = name;
+        //    BatteryLevel = batteryLevel;
+        //}
+        public virtual double GetBatteryPercentage()
+        {
+            return (CurrentPowerKWH / PowerCapacityKWH) * 100;
+        }
+        public abstract string DescribeRobot();
+        public virtual string DisplayBatteryInformation()
+        {
+            return $"Battery Information\nCapacity: {PowerCapacityKWH}kWh\nCurrent Power: {CurrentPowerKWH}kWh\nBattery Level: {GetBatteryPercentage():F2}%";
+        }
+
+        public override string ToString()
+        {
+            return $"{RobotName} - ";
+        }
+    }
+}
